664cc5e985ad97aa3d10de7c48505f0f9dd824fe
Martipolo <martipolo@gmail.com>
2024-01-15 15:53:10 +0100

    Ajout test gagnant Update

--- testu/test_gagnant.py
+++ testu/test_gagnant.py
@@ -1,26 +1,14 @@
 from noyau import *
 import pytest
 
-def test_coup_gagnant():
+def test_gagnant():
     # Stub
     def jeton_stub(joueur, posx, posy, depx, depy, grille):
-        # Replace this stub with your logic or an actual implementation of the jeton function
+        
         return 0
 
-    # Test case 1: No winning move
-    grille1 = [
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-    ]
-    joueur1 = 0
-    i1, j1 = 2, 3
-    assert not coup_gagnant(joueur1, i1, j1, grille1, jeton_stub)
-
-    # Test case 2: Winning move in vertical direction
+    
+    # Test 1: gagnant à la verticale
     grille2 = [
         [1, 0, 0, 0, 0, 0, 0],
         [1, 0, 0, 0, 0, 0, 0],
@@ -31,9 +19,9 @@ def test_coup_gagnant():
     ]
     joueur2 = 1
     i2, j2 = 3, 0
-    assert coup_gagnant(joueur2, i2, j2, grille2, jeton_stub)
+    assert coup_gagnant(joueur2, i2, j2, grille2)
 
-    # Test case 3: Winning move in horizontal direction
+    
     grille3 = [
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
@@ -44,7 +32,4 @@ def test_coup_gagnant():
     ]
     joueur3 = 1
     i3, j3 = 2, 2
-    assert coup_gagnant(joueur3, i3, j3, grille3, jeton_stub)
-
-# Run the tests
-test_coup_gagnant()
+    assert coup_gagnant(joueur3, i3, j3, grille3)
